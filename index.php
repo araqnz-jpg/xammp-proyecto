2f9dbdd1aca456e177785b21b32a14f04128fccf
araqnz-jpg <araqnz@gmail.com>
2026-05-24 18:09:29 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<form action="resultados.php" method="POST">
+
+    <label>Nombre:</label>
+    <input type="text" name="nombre">
+
+    <label>Correo:</label>
+    <input type="email" name="correo">
+
+    <label>Edad:</label>
+    <input type="text" name="edad">
+
+    <button type="submit">
+        Ingresar datos
+    </button>
+
+</form>
